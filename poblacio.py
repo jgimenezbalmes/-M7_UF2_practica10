8e9e04079322c792ffb1877b7bf6f150873795ad
jgimenezbalmes <jgimenez@jaumebalmes.net>
2023-01-12 20:14:44 +0100

    primer grafic

--- poblacio.py
+++ poblacio.py
@@ -1,5 +1,12 @@
 import pandas as pd
+import matplotlib.pyplot as plt
 
-def poblacio(){
+df = pd.read_csv('ciutats.csv', usecols = ['City', 'Population'], nrows=11)
 
-}
+df2 = df.dropna()
+df2['Population'] = df2['Population'].apply(lambda x : x.replace(',',''))
+df2['Population'] = df2['Population'].astype(int)
+plt.pie(df2["Population"], labels=df2['City'], autopct='%1.1f%%')
+def graf1():
+    plt.show()
+
