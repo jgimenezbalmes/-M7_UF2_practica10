4ee41ca5b25dd0179eec1db7a4afb688ae475f94
jgimenezbalmes <jgimenez@jaumebalmes.net>
2023-01-12 20:41:25 +0100

    intent de tots els grafics (es solapen)

---
+++ densitatm.py
@@ -0,0 +1,12 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+
+df5 = pd.read_csv('ciutats.csv', usecols = ['City', 'Density  M2'], nrows=11)
+
+df6 = df5.dropna()
+df6['Density  M2'] = df6['Density  M2'].apply(lambda x : x.replace(',',''))
+df6['Density  M2'] = df6['Density  M2'].astype(int)
+plt.pie(df6['Density  M2'], labels=df6['City'], autopct='%1.1f%%')
+
+def graf3():
+    plt.show()
--- main.py
+++ main.py
@@ -1,3 +1,11 @@
-import poblacio as pb
+if __name__ == "__main__":
+    import poblacio as pb
+    import densitatkm as dkm
+    import densitatm as dm
+
 
 pb.graf1()
+dkm.graf2()
+dm.graf3()
+
+
